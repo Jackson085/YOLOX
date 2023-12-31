0f15b4232e3b6399383ea942db154b71cbcc2384
Jackson085 <jackson26594916@gmail.com>
2023-12-31 23:35:35 +0100

    change parser

--- tools/train.py
+++ tools/train.py
@@ -17,83 +17,25 @@ from yolox.utils import configure_module, configure_nccl, configure_omp, get_num
 
 def make_parser():
     parser = argparse.ArgumentParser("YOLOX train parser")
-    parser.add_argument("-expn", "--experiment-name", type=str, default=None)
+    parser.add_argument("-expn", "--experiment-name", type=str, default="Test002")
     parser.add_argument("-n", "--name", type=str, default=None, help="model name")
 
     # distributed
-    parser.add_argument(
-        "--dist-backend", default="nccl", type=str, help="distributed backend"
-    )
-    parser.add_argument(
-        "--dist-url",
-        default=None,
-        type=str,
-        help="url used to set up distributed training",
-    )
-    parser.add_argument("-b", "--batch-size", type=int, default=64, help="batch size")
-    parser.add_argument(
-        "-d", "--devices", default=None, type=int, help="device for training"
-    )
-    parser.add_argument(
-        "-f",
-        "--exp_file",
-        default=None,
-        type=str,
-        help="plz input your experiment description file",
-    )
-    parser.add_argument(
-        "--resume", default=False, action="store_true", help="resume training"
-    )
+    parser.add_argument("--dist-backend", default="nccl", type=str, help="distributed backend")
+    parser.add_argument("--dist-url", default=None, type=str, help="url used to set up distributed training")
+    parser.add_argument("-b", "--batch-size", type=int, default=8, help="batch size")
+    parser.add_argument("-d", "--devices", default=1, type=int, help="device for training")
+    parser.add_argument("-f", "--exp_file", default=r'exps/example/yolox_voc/yolox_voc_s.py', type=str, help="plz input your experiment description file")
+    parser.add_argument("--resume", default=False, action="store_true", help="resume training")
     parser.add_argument("-c", "--ckpt", default=None, type=str, help="checkpoint file")
-    parser.add_argument(
-        "-e",
-        "--start_epoch",
-        default=None,
-        type=int,
-        help="resume training start epoch",
-    )
-    parser.add_argument(
-        "--num_machines", default=1, type=int, help="num of node for training"
-    )
-    parser.add_argument(
-        "--machine_rank", default=0, type=int, help="node rank for multi-node training"
-    )
-    parser.add_argument(
-        "--fp16",
-        dest="fp16",
-        default=False,
-        action="store_true",
-        help="Adopting mix precision training.",
-    )
-    parser.add_argument(
-        "--cache",
-        type=str,
-        nargs="?",
-        const="ram",
-        help="Caching imgs to ram/disk for fast training.",
-    )
-    parser.add_argument(
-        "-o",
-        "--occupy",
-        dest="occupy",
-        default=False,
-        action="store_true",
-        help="occupy GPU memory first for training.",
-    )
-    parser.add_argument(
-        "-l",
-        "--logger",
-        type=str,
-        help="Logger to be used for metrics. \
-        Implemented loggers include `tensorboard` and `wandb`.",
-        default="tensorboard"
-    )
-    parser.add_argument(
-        "opts",
-        help="Modify config options using the command-line",
-        default=None,
-        nargs=argparse.REMAINDER,
-    )
+    parser.add_argument("-e", "--start_epoch", default=None, type=int, help="resume training start epoch",)
+    parser.add_argument("--num_machines", default=1, type=int, help="num of node for training")
+    parser.add_argument("--machine_rank", default=0, type=int, help="node rank for multi-node training")
+    parser.add_argument("--fp16", dest="fp16", default=False, action="store_true", help="Adopting mix precision training.")
+    parser.add_argument("--cache", type=str, nargs="?", const="ram", help="Caching imgs to ram/disk for fast training.")
+    parser.add_argument("-o", "--occupy", dest="occupy", default=False, action="store_true", help="occupy GPU memory first for training.")
+    parser.add_argument("-l", "--logger", type=str, help="Logger to be used for metrics. Implemented loggers include `tensorboard` and `wandb`.", default="tensorboard")
+    parser.add_argument("opts", help="Modify config options using the command-line", default=None, nargs=argparse.REMAINDER)
     return parser
 
 
@@ -121,9 +63,12 @@ def main(exp: Exp, args):
 if __name__ == "__main__":
     configure_module()
     args = make_parser().parse_args()
-    exp = get_exp(args.exp_file, args.name)
+    from exps.example.yolox_voc.yolox_voc_s import Exp
+
+    exp = Exp()
     exp.merge(args.opts)
     check_exp_value(exp)
+    logger.critical(args.batch_size)
 
     if not args.experiment_name:
         args.experiment_name = exp.exp_name
